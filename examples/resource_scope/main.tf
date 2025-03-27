8a7ed17981a731f11ff618df0423f65ebd3cebf5
Rhys Ash <rhysash@microsoft.com>
2025-03-27 16:52:14 +0000

    Add Resource Scope example for Maintenance Configuration module

---
+++ examples/resource_scope/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.13, < 3"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.21"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {}
+
+## Section to provide a random Azure region for the resource group
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "~> 0.1"
+}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+## End of section to provide a random Azure region for the resource group
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+# This is the module call
+# Do not specify location here due to the randomization above.
+# Leaving location as `null` will cause the module to use the resource group location
+# with a data source.
+module "test" {
+  source = "../../"
+  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
+  # ...
+  location = azurerm_resource_group.this.location
+  name     = var.name
+  scope    = "Resource"
+  extension_properties = {
+    maintenanceSubScope = "NetworkGatewayMaintenance"
+  }
+  resource_group_name = azurerm_resource_group.this.name
+  enable_telemetry    = var.enable_telemetry
+}
---
+++ examples/resource_scope/variables.tf
@@ -0,0 +1,15 @@
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see <https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+}
+
+variable "name" {
+  type        = string
+  default     = "mc-resource-scope"
+  description = "(Optional) The name of the the maintenance configuration."
+}
